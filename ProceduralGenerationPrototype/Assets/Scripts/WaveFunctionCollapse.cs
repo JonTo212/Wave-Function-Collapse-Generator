b02b748e805a04373cc6a23d94427b4b1a2bc48c
JonTo212 <117186718+JonTo212@users.noreply.github.com>
2024-09-16 14:07:26 -0400

    added edge cases to start creating box-esque shape

--- ProceduralGenerationPrototype/Assets/Scripts/WaveFunctionCollapse.cs
+++ ProceduralGenerationPrototype/Assets/Scripts/WaveFunctionCollapse.cs
@@ -6,7 +6,6 @@ public enum TileType
     Floor,
     Wall,
     Corner
-    // Add more tile types as needed
 }
 
 public class TileConstraints
@@ -15,20 +14,19 @@ public class TileConstraints
 
     public TileConstraints()
     {
-        // Example constraints
-        Constraints[TileType.Floor] = new List<TileType> { TileType.Wall, TileType.Floor, TileType.Corner };
-        Constraints[TileType.Wall] = new List<TileType> { TileType.Corner, TileType.Floor }; // Walls should only be next to walls
-        Constraints[TileType.Corner] = new List<TileType> { TileType.Wall, TileType.Corner };
+        Constraints[TileType.Floor] = new List<TileType> { TileType.Wall, TileType.Floor, TileType.Corner }; //floors can be beside anything
+        Constraints[TileType.Wall] = new List<TileType> { TileType.Floor, TileType.Wall }; //walls can only be beside floors and walls
+        Constraints[TileType.Corner] = new List<TileType> { TileType.Wall }; //corners can only be beside walls
     }
 }
 
 public class WaveFunctionCollapse : MonoBehaviour
 {
-    public int width = 10;
-    public int length = 10;
-    public TileType[,] grid;
-    public TileConstraints constraints = new TileConstraints();
-    public GameObject[] tilePrefabs; // Assign prefabs for each tile type in Unity Inspector
+    public int width = 10; //width of grid
+    public int length = 10; //length of grid
+    public TileType[,] grid; //2d array (e.g. (x,y) array)
+    public TileConstraints constraints = new TileConstraints(); //Checks constraints
+    public GameObject[] tilePrefabs; //actual tile objects to be instantiated
 
     void Start()
     {
@@ -39,49 +37,58 @@ public class WaveFunctionCollapse : MonoBehaviour
 
     void GenerateGrid()
     {
-        // Initialize the grid with empty tiles
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < length; z++)
             {
-                grid[x, z] = TileType.Floor;
+                grid[x, z] = TileType.Floor; //base grid is all floors
             }
         }
 
-        // Perform WFC algorithm
+        //set edges to be walls
+        for (int x = 0; x < width; x++)
+        {
+            grid[x, 0] = TileType.Wall; // Bottom edge
+            grid[x, length - 1] = TileType.Wall; // Top edge
+        }
+
+        for (int z = 0; z < length; z++)
+        {
+            grid[0, z] = TileType.Wall; // Left edge
+            grid[width - 1, z] = TileType.Wall; // Right edge
+        }
+
         Collapse();
     }
 
     void Collapse()
     {
-        // Simplified collapse algorithm: randomly assign tiles while respecting constraints
-        for (int x = 0; x < width; x++)
+        for (int x = 1; x < width - 1; x++) //start at 1 and end at width - 1 because edges are already pre-set
         {
-            for (int z = 0; z < length; z++)
+            for (int z = 1; z < length - 1; z++)
             {
                 List<TileType> possibleTiles = GetPossibleTiles(x, z);
                 if (possibleTiles.Count > 0)
                 {
-                    grid[x, z] = possibleTiles[Random.Range(0, possibleTiles.Count)];
+                    grid[x, z] = possibleTiles[Random.Range(0, possibleTiles.Count)]; //randomly select from possible tiles, fill grid with tiles
                 }
             }
         }
     }
 
-    List<TileType> GetPossibleTiles(int x, int y)
+    List<TileType> GetPossibleTiles(int x, int z) //feed the x and z value of grid
     {
-        List<TileType> possibleTiles = new List<TileType>();
+        List<TileType> possibleTiles = new List<TileType>(); //creates a list of tile types that will be filled with the possible tiles that can be instantiated in each grid slot
 
-        // Check constraints with neighboring tiles
-        foreach (TileType tileType in System.Enum.GetValues(typeof(TileType)))
+        foreach (TileType tileType in System.Enum.GetValues(typeof(TileType))) //for each tile type
         {
-            bool isValid = true;
-            if (x > 0 && !constraints.Constraints[tileType].Contains(grid[x - 1, y])) isValid = false; // Left
-            if (y > 0 && !constraints.Constraints[tileType].Contains(grid[x, y - 1])) isValid = false; // Down
+            bool isValid = true; 
+            if (x > 0 && !constraints.Constraints[tileType].Contains(grid[x - 1, z])) isValid = false; //checks left neighbouring tile to see if the current tile type is valid
+            if (z > 0 && !constraints.Constraints[tileType].Contains(grid[x, z - 1])) isValid = false; //checks bottom neighbouring tile to see if current tile type is valid
 
             if (isValid)
             {
-                possibleTiles.Add(tileType);
+                possibleTiles.Add(tileType); //adds possible tile types to the list of potential tiles for slot (x,z)
             }
         }
 
@@ -94,9 +101,68 @@ public class WaveFunctionCollapse : MonoBehaviour
         {
             for (int z = 0; z < length; z++)
             {
-                GameObject tilePrefab = tilePrefabs[(int)grid[x, z]];
-                Instantiate(tilePrefab, new Vector3(x, 0, z), Quaternion.identity);
+                GameObject instantiatedTile = tilePrefabs[(int)grid[x, z]]; //get the corresponding tile at each grid slot
+                Quaternion rotation = GetRotationForTile(x, z); //get rotation for walls and corners
+                Instantiate(instantiatedTile, new Vector3(x, 0, z), rotation); //instantiate tile prefabs with rotation
             }
         }
+    }
+
+    Quaternion GetRotationForTile(int x, int z)
+    {
+        TileType tile = grid[x, z];
+        Quaternion rotation = Quaternion.identity;
+
+        if (tile == TileType.Wall)
+        {
+            if (x == 0) //left edge
+            {
+                rotation = Quaternion.Euler(0, 270, 0);
+            }
+            else if (x == width - 1) //right edge
+            {
+                rotation = Quaternion.Euler(0, 90, 0);
+            }
+            else if(z == 0) //top edge
+            {
+                rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else if (z == length - 1) //bottom edge
+            {
+                rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                if (x > 0 && grid[x - 1, z] == TileType.Floor) //if there's a floor to the left, rotate the wall to be vertical, otherwise leave it horizontal
+                {
+                    rotation = Quaternion.Euler(0, 90, 0);
+                }
+                else
+                {
+                    rotation = Quaternion.Euler(0, 0, 0);
+                }
+            }
+        }
+
+        else if (tile == TileType.Corner)
+        {
+            bool hasWallOnLeft = (x > 0 && grid[x - 1, z] == TileType.Wall);
+            bool hasWallOnBottom = (z > 0 && grid[x, z - 1] == TileType.Wall);
+
+            if (hasWallOnLeft && hasWallOnBottom)
+            {
+                rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (hasWallOnLeft)
+            {
+                rotation = Quaternion.Euler(0, 90, 0); //rotate 90 degrees if there's a wall on the left
+            }
+            else if (hasWallOnBottom)
+            {
+                rotation = Quaternion.Euler(0, 180, 0); //rotate 180 degrees if there's a wall on the bottom
+            }
+        }
+
+        return rotation;
     }
 }
