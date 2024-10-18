6948b1828d663e3ce8154625f0f0d3e625223d89
Jonathan To <jonto398@gmail.com>
2024-10-18 00:58:02 -0400

    Converted WFCGenerator and WFCNodes to 3D
    -Converted 2D elements (2D array, Vector2Int) to 3D (3D array, Vector3Int) in WFC generator
    -Added viableFowardNodes and viableBackwardNodes lists to WFC node

--- ProceduralGenerationPrototype/Assets/Scripts/WFCNode/WFCGenerator.cs
+++ ProceduralGenerationPrototype/Assets/Scripts/WFCNode/WFCGenerator.cs
@@ -1,4 +1,4 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,34 +7,32 @@ public class WFCGenerator : MonoBehaviour
     [Header("Grid Parameters")]
     [SerializeField] private int gridWidth;
     [SerializeField] private int gridHeight;
+    [SerializeField] private int gridDepth;
 
-    private WFCNode[,] grid;
+    private WFCNode[,,] grid; //3D array
 
     [Header("Node Parameters")]
 
     [SerializeField] private List<WFCNode> nodes;                    //list of all nodes
-    private List<Vector2Int> toCollapse = new List<Vector2Int>();    //list of nodes that still need to be collapsed
+    private List<Vector3Int> toCollapse = new List<Vector3Int>();    //list of nodes that still need to be collapsed
 
-    private Vector2Int[] neighbourCoordinates = new Vector2Int[]
+    private Vector3Int[] neighbourCoordinates = new Vector3Int[]
     {
-        new Vector2Int(0, 1),          //up
-        new Vector2Int(0, -1),         //down
-        new Vector2Int(-1, 0),         //left
-        new Vector2Int(1, 0),          //right
+        new Vector3Int(0, 1, 0),          //up
+        new Vector3Int(0, -1, 0),         //down
+        new Vector3Int(-1, 0, 0),         //left
+        new Vector3Int(1, 0, 0),          //right
+        new Vector3Int(0, 0, 1),          //forward
+        new Vector3Int(0, 0, -1)          //backward
     };
 
     #region Helper Functions
 
-    private bool IsInsideGrid(Vector2Int gridPos) //check whether a given node's position is inside the grid (to ensure no checks out of bounds)
+    private bool IsInsideGrid(Vector3Int gridPos) //check whether a given node's position is inside the grid (to ensure no checks out of bounds)
     {
-        if (gridPos.x >= 0 && gridPos.x < gridWidth && gridPos.y >= 0 && gridPos.y < gridHeight)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return gridPos.x >= 0 && gridPos.x < gridWidth &&
+           gridPos.y >= 0 && gridPos.y < gridHeight &&
+           gridPos.z >= 0 && gridPos.z < gridDepth;
     }
 
     private void ReducePossibleNodes(List<WFCNode> potentialNodes, List<WFCNode> validNodes) //remove all nodes that are invalid
@@ -95,39 +93,42 @@ public class WFCGenerator : MonoBehaviour
 
     private void InitializeGrid()
     {
-        grid = new WFCNode[gridWidth, gridHeight];
+        grid = new WFCNode[gridWidth, gridHeight, gridDepth];
     }
 
     private void CollapseGrid()
     {
         toCollapse.Clear();
 
-        toCollapse.Add(new Vector2Int(gridWidth / 2, gridHeight / 2)); //start with middle tile
+        toCollapse.Add(new Vector3Int(gridWidth / 2, gridHeight / 2, gridDepth / 2)); //start with central tile
 
         while (toCollapse.Count > 0)
         {
             int x = toCollapse[0].x;
             int y = toCollapse[0].y;
+            int z = toCollapse[0].z;
 
             List<WFCNode> potentialNodes = new List<WFCNode>(nodes); //create a new list with all of the nodes to begin with
 
             for (int i = 0; i < neighbourCoordinates.Length; i++) //loop through all neighbours (i.e. up, down, left, right)
             {
-                Vector2Int neighbour = new Vector2Int(x + neighbourCoordinates[i].x, y + neighbourCoordinates[i].y); //this represents each neighbour position
+                Vector3Int neighbour = new Vector3Int(x + neighbourCoordinates[i].x, y + neighbourCoordinates[i].y, z + neighbourCoordinates[i].z); //this represents each neighbour position
 
                 if (IsInsideGrid(neighbour))
                 {
-                    WFCNode neighbourNode = grid[neighbour.x, neighbour.y];
+                    WFCNode neighbourNode = grid[neighbour.x, neighbour.y, neighbour.z];
 
                     if (neighbourNode != null) //if the neighbour node is null, it needs to be collapsed still
                     {
 
                         WFCConnection[] connections = new WFCConnection[]
                         {
-                            neighbourNode.viableBottomNodes,  // index 0 = top node, so the corresponding nodes to check are viable bottom nodes
-                            neighbourNode.viableTopNodes,     // index 1 = bottom node, so the corresponding nodes to check are viable top nodes
-                            neighbourNode.viableRightNodes,   // index 2 = left node, so the corresponding nodes to check are viable right nodes
-                            neighbourNode.viableLeftNodes     // index 3 = right node, so the corresponding nodes to check are viable left nodes
+                            neighbourNode.viableBottomNodes,   // index 0 = top node, so the corresponding nodes to check are viable bottom nodes
+                            neighbourNode.viableTopNodes,      // index 1 = bottom node, so the corresponding nodes to check are viable top nodes
+                            neighbourNode.viableRightNodes,    // index 2 = left node, so the corresponding nodes to check are viable right nodes
+                            neighbourNode.viableLeftNodes,     // index 3 = right node, so the corresponding nodes to check are viable left nodes
+                            neighbourNode.viableBackwardNodes, // index 4 = forward node, so the corresponding nodes to check are viable back nodes
+                            neighbourNode.viableForwardNodes   // index 5 = backward node, so the corresponding nodes to check are viable forward nodes
                         };
 
                         ReducePossibleNodes(potentialNodes, connections[i].compatibleNodes); //reduce possible nodes for this tile
@@ -145,16 +146,16 @@ public class WFCGenerator : MonoBehaviour
 
             if (potentialNodes.Count < 1) //no possible tiles based on constraints -> this can be changed if desired
             {
-                grid[x, y] = nodes[0]; //if no other possibilities, just add a floor
+                grid[x, y, z] = nodes[0]; //if no other possibilities, just add a floor
                 //Debug.Log("Attempted to collapse on " + x + ", " + y + " but no compatible tiles.");
             }
             else
             {
-                grid[x, y] = potentialNodes[Random.Range(0, potentialNodes.Count)]; //choose random node
+                grid[x, y, z] = potentialNodes[Random.Range(0, potentialNodes.Count)]; //choose random node
                 //grid[x, y] = GetHighestWeightNode(potentialNodes); //choose the highest weighted node
             }
 
-            GameObject newNode = Instantiate(grid[x, y].prefab, new Vector3(x, 0, y), Quaternion.identity);
+            GameObject newNode = Instantiate(grid[x, y, z].prefab, new Vector3(x, y, z), Quaternion.identity);
             //grid[x, y].instantiatedObject = newNode; //for some reason this doesn't work
             toCollapse.RemoveAt(0);
         }
--- ProceduralGenerationPrototype/Assets/Scripts/WFCNode/WFCNode.cs
+++ ProceduralGenerationPrototype/Assets/Scripts/WFCNode/WFCNode.cs
@@ -14,10 +14,13 @@ public class WFCNode : ScriptableObject
     public WFCConnection viableBottomNodes;
     public WFCConnection viableLeftNodes;
     public WFCConnection viableRightNodes;
+    public WFCConnection viableForwardNodes;
+    public WFCConnection viableBackwardNodes;
 
     //called when changes happen in editor
     private void OnValidate()
     {
+        AutoRemoveOppositeConnections();
         AutoPopulateOppositeConnections();
     }
 
@@ -53,6 +56,74 @@ public class WFCNode : ScriptableObject
             if (!rightNode.viableLeftNodes.compatibleNodes.Contains(this))
             {
                 rightNode.viableLeftNodes.compatibleNodes.Add(this);
+            }
+        }
+
+        foreach (var forwardNode in viableForwardNodes.compatibleNodes)
+        {
+            if (!forwardNode.viableBackwardNodes.compatibleNodes.Contains(this))
+            {
+                forwardNode.viableBackwardNodes.compatibleNodes.Add(this);
+            }
+        }
+
+        foreach (var backwardNode in viableBackwardNodes.compatibleNodes)
+        {
+            if (!backwardNode.viableForwardNodes.compatibleNodes.Contains(this))
+            {
+                backwardNode.viableForwardNodes.compatibleNodes.Add(this);
+            }
+        }
+    }
+
+    //auto-populate the equivalent node
+    private void AutoRemoveOppositeConnections()
+    {
+        foreach (var topNode in viableTopNodes.compatibleNodes)
+        {
+            if (topNode.viableBottomNodes.compatibleNodes.Contains(this))
+            {
+                topNode.viableBottomNodes.compatibleNodes.Remove(this);
+            }
+        }
+
+        foreach (var bottomNode in viableBottomNodes.compatibleNodes)
+        {
+            if (bottomNode.viableTopNodes.compatibleNodes.Contains(this))
+            {
+                bottomNode.viableTopNodes.compatibleNodes.Remove(this);
+            }
+        }
+
+        foreach (var leftNode in viableLeftNodes.compatibleNodes)
+        {
+            if (leftNode.viableRightNodes.compatibleNodes.Contains(this))
+            {
+                leftNode.viableRightNodes.compatibleNodes.Remove(this);
+            }
+        }
+
+        foreach (var rightNode in viableRightNodes.compatibleNodes)
+        {
+            if (rightNode.viableLeftNodes.compatibleNodes.Contains(this))
+            {
+                rightNode.viableLeftNodes.compatibleNodes.Remove(this);
+            }
+        }
+
+        foreach (var forwardNode in viableForwardNodes.compatibleNodes)
+        {
+            if (forwardNode.viableBackwardNodes.compatibleNodes.Contains(this))
+            {
+                forwardNode.viableBackwardNodes.compatibleNodes.Remove(this);
+            }
+        }
+
+        foreach (var backwardNode in viableBackwardNodes.compatibleNodes)
+        {
+            if (backwardNode.viableForwardNodes.compatibleNodes.Contains(this))
+            {
+                backwardNode.viableForwardNodes.compatibleNodes.Remove(this);
             }
         }
     }
